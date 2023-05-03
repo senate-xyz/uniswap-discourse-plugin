0fd547f8c0ed76522daf65f7424651f6df56fd2a
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-05-03 12:52:01 +0300

    cleanup

--- assets/javascripts/discourse/initializers/senate-button.js
+++ assets/javascripts/discourse/initializers/senate-button.js
@@ -1,56 +1,60 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { h } from "virtual-dom";
-import { popupAjaxError } from "discourse/lib/ajax-error";
+import { iconNode } from "discourse-common/lib/icon-library";
+
+function createTooltipButton(helper) {
+  let icon = iconNode("envelope");
+  let isVisible = false;
+
+  return helper.h("div", {}, [
+    helper.h(
+      "button.tooltip-btn",
+      {
+        onclick: () => {
+          isVisible = !isVisible;
+        },
+        style: {
+          height: "32px",
+          width: "256px",
+          color: "#E9337A",
+          background: "#FFE9F1",
+          border: "1px solid #F2ACC7",
+          borderRadius: "16px",
+          cursor: "pointer",
+          padding: "0.5em 1em",
+        },
+      },
+      helper.h(
+        "div",
+        { style: { display: "flex", alignItems: "center", gap: "0.5em" } },
+        [icon, "Setup Proposal Notifications"]
+      )
+    ),
+    helper.h(
+      "div.tooltip-content",
+      { style: { display: isVisible ? "block" : "none" } },
+      [h("p", "This is a test tooltip")]
+    ),
+  ]);
+
+  function render() {
+    const container = document.querySelector(".tooltip-btn-container");
+    if (container) {
+      helper.mount(container, createTooltipButton(helper));
+    }
+  }
+}
 
 export default {
   name: "discourse-tooltip-button",
 
   initialize() {
     withPluginApi("0.8.20", (api) => {
-      api.decorateWidget("header-icons:before", (helper) => {
-        const state = helper.widget.state;
-        const button = helper.h("button.tooltip-btn", "Tooltip Button");
-
-        const tooltip = h(
-          "div.tooltip-content",
-          { style: { display: state.showTooltip ? "block" : "none" } },
-          [h("p", "This is a test tooltip")]
-        );
-
-        return [button, tooltip];
-      });
-
-      api.attachWidgetAction("header-icons", "click", function (event) {
-        const target = event.target;
-        const tooltipBtn = this.$(".tooltip-btn")[0];
-
-        if (target === tooltipBtn) {
-          this.state.showTooltip = !this.state.showTooltip;
-          this.scheduleRerender();
-          event.stopPropagation();
-        } else if (this.state.showTooltip) {
-          this.state.showTooltip = false;
-          this.scheduleRerender();
-        }
-      });
-
-      api.addPopupMenuOptionsCallback(() => {
-        if (this.state.showTooltip) {
-          this.state.showTooltip = false;
-          this.scheduleRerender();
-        }
-      });
-
-      api.addGlobalClickListener((event) => {
-        const target = event.target;
-        const tooltipContent = this.$(".tooltip-content")[0];
-
-        if (tooltipContent && !tooltipContent.contains(target)) {
-          const headerIcons = api.container.lookup("widget:header-icons");
-          if (headerIcons.state.showTooltip) {
-            headerIcons.state.showTooltip = false;
-            headerIcons.scheduleRerender();
-          }
+      api.decorateWidget("header-buttons:before", (helper) => {
+        if (api.getCurrentUser()) {
+          return helper.h("div.tooltip-btn-container", [
+            createTooltipButton(helper),
+          ]);
         }
       });
     });
